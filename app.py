5193e100133c7e52127799bd0dd7b4bea8a6f514
PradeepYadav90 <pradeepydv2015@gmail.com>
2025-08-02 15:43:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+users = ["Alice", "Bob", "Charlie"]
+
+@app.route('/')
+def home():
+    return render_template("index.html", users=users)
+
+@app.route('/add-user', methods=['POST'])
+def add_user():
+    name = request.form.get("name")
+    if name:
+        users.append(name)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
